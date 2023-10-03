cf4ef9532fa215be14ddd60642df63325437e55d
margaretanp <143424663+margaretanp@users.noreply.github.com>
2023-10-03 11:30:36 +0700

    Add files via upload

---
+++ Pemilihan2Percobaan119.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan119 {
+
+    public static void main(String[] args) {
+        Scanner input19 = new Scanner(System.in);
+        System.out.println("Masukkan tahun: ");
+        int tahun = input19.nextInt();
+
+        if((tahun % 4) == 0) {
+            if((tahun % 100) !=0)
+            System.out.println("Tahun Kabisat");
+        } else
+        System.out.println("Bukan Tahun Kabisat");
+    }
+}
